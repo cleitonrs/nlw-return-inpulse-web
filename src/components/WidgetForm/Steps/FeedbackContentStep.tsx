35044d11125550b692e46816ff0cd7524268b41f
cleitonrs <rscleitonrs@gmail.com>
2022-05-03 16:18:26 -0300

    screenshot-button-component

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,7 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType
@@ -38,6 +39,21 @@ export const FeedbackContentStep = ({
           className="min-w=[304px] w-full min-h=[112px] text-sm placeholder-zinc-400 text-zinc 100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo..."
         />
+
+        <footer className="flex gap-2 mt-2">
+
+          <ScreenshotButton />
+
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar Feedback
+
+
+          </button>
+        </footer>
+
       </form>
     </>
   );
