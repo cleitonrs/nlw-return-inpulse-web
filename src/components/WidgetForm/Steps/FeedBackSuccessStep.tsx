76c6de5f7f46fec4e0c6599b7127b3fb7222c7ea
cleitonrs <rscleitonrs@gmail.com>
2022-06-29 10:52:58 -0300

    fixes

--- src/components/WidgetForm/Steps/FeedBackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedBackSuccessStep.tsx
@@ -35,7 +35,7 @@ export const FeedBackSuccessStep = ({ onFeedbackRestartRequested }: FeedBackSucc
           type="button"
           onClick={onFeedbackRestartRequested}
           className="py-2 px-6 mt-6 bg-zinc-200 dark:bg-zinc-800 text-zinc-800 
-          dark:text-zinc-100 rounded-md border-transparent text-sm leading-6 hover:bg-slate-400 dark:hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+          dark:text-zinc-100 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-400 dark:hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
         >
           Quero enviar outro
         </button>
