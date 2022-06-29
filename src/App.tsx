4892e5f985d5dcd4aa3b39ebeea70aa27f9ad41a
cleitonrs <rscleitonrs@gmail.com>
2022-06-29 08:44:12 -0300

    theme mode setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from 'react'
 import { Widget } from './components/Widget'
 
 export const App = () => {
+  const [theme, setTheme] = useState('')
+
+  useEffect(() => {
+    if(window.matchMedia('prefers-color-scheme: dark').matches) {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }, [])
+
+  useEffect(() => {
+    if(theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
+
   return (
     <div>
       <Widget />
